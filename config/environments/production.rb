433d1e484a8b75111fb323d6526e0d9ae14f3571
YoussefAoun <youssef.aoun@sowlutions.com>
2025-03-14 16:31:53 +0200

    Fix SolidCache database issue for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,7 @@ Rails.application.configure do
   config.active_support.report_deprecations = false
 
   # Replace the default in-process memory cache store with a durable alternative.
-  config.cache_store = :solid_cache_store, { database: { writing: :primary }, backend: :postgresql }
+  # config.cache_store = :solid_cache_store, { database: { writing: :primary }, backend: :postgresql }
 
 
 
